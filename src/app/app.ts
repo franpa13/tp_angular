6c32f14b6c4b25d7961c83652411d3101b714216
Francisco Javier Paredes <frandelregue@gmail.com>
2026-05-07 23:46:06 -0300

    add new library flowbite

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Punto1 } from "./components/punto1/punto1";
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import { Punto1 } from "./components/punto1/punto1";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('tp3');
+
+  ngAfterViewInit(): void {
+    initFlowbite();
+  }
 }
